7a06d4fffe1d7e03fc25cd29da8813e65fedc5ba
SathishKumar Eswaran <seswar@microsoft.com>
2018-05-30 07:18:10 -0700

    Added support for edx_trusted_application

--- edx_rest_framework_extensions/permissions.py
+++ edx_rest_framework_extensions/permissions.py
@@ -1,9 +1,7 @@
 """ Permission classes. """
 from rest_framework.permissions import BasePermission
-from provider.oauth2.models import AccessToken as DOPAccessToken
 from edx_rest_framework_extensions.utils import jwt_decode_handler, is_token_version_incompatable
 from openedx.core.djangoapps.oauth_dispatch.utils import is_oauth_scope_enforcement_enabled
-
 
 class IsSuperuser(BasePermission):
     """ Allows access only to superusers. """
@@ -11,14 +9,13 @@ class IsSuperuser(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_superuser
 
-
 class HasScopedToken(BasePermission):
     """
     The request is authenticated and has the required scopes and organization filters
     """
     def _token_filters(self, decoded_token):
         # get filters list from jwt token and return dict
-        if 'filters' in decoded_token:
+        if decoded_token.has_key('filters'):
             filters_list = decoded_token['filters']
             filters = {}
             for each in filters_list:
@@ -27,7 +24,7 @@ class HasScopedToken(BasePermission):
                     filters[each[0]].append(each[1])
                 else:
                     filters[each[0]] = [each[1]]
-            return filters
+        return filters
 
     def has_permission(self, request, view):
         """
@@ -39,7 +36,7 @@ class HasScopedToken(BasePermission):
             # check to see if token is a DOP token
             # if so this represents a client which is implicitly trusted
             # (since it is an internal Open edX application)
-            if isinstance(token, DOPAccessToken):
+            if decoded_token['edx_trusted_application']:
                 return True
 
             if is_token_version_incompatable(decoded_token['version']):
